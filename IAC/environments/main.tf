238d3cfe01a20796fd476e5787e56a8582cb714b
Oleksii Sheludchenko <oshelu@softserveinc.com>
2020-08-28 13:55:24 +0300

    Initial

--- IAC/environments/main.tf
+++ IAC/environments/main.tf
@@ -6,14 +6,6 @@
 #   sqlsvr_admin_password = var.sqlsvr_admin_password
 # }
 
-# module "stage" {
-#   source = "../modules/memos_app"
-  
-#   env                   = "Stage"
-#   product               = var.product
-#   sqlsvr_admin_password = var.sqlsvr_admin_password
-# }
-
 # module "prod" {
 #   source = "../modules/memos_app"
   
